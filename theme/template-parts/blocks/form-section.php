469af7a449623a9cd54a0d6ad19ccbebe7fb4018
ValeryZhitnikov <valeryzhitnikov91@gmail.com>
2025-08-04 15:13:52 +0300

    Fix form

--- theme/template-parts/blocks/form-section.php
+++ theme/template-parts/blocks/form-section.php
@@ -2,9 +2,8 @@
   <div class="h1 main-section__title">
     Заявка
   </div>
-  <?php //get_template_part('template-parts/components/main-form'); ?>
   <?php get_template_part('template-parts/components/main-form-cf7', null, [
-    'cf7_shortcode' => '[contact-form-7 id="321c422" title="Контактная форма 1"]'
+    'cf7_shortcode' => '[contact-form-7 id="65bb0a0" title="Контактная форма"]'
     ]); ?>
   <?php get_template_part('template-parts/UI/gradient-svg'); ?>
 </section>
